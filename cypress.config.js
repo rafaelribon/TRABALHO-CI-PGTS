435c770bd2172e5239508fbd0893bd728b47cfff
Rafael Grassi Ribon <rafael.ribon@gpsafira.com.br>
2024-11-09 15:29:43 -0300

    Ajuste no arquivo cypress.config.js para gerar relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,18 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports', // Diretório onde os relatórios serão salvos
+    overwrite: false, // Não sobrescreve relatórios anteriores
+    html: true, // Gera relatório HTML
+    json: true, // Gera relatório JSON
+    charts: true, // Inclui gráficos no relatório HTML
+    reportFilename: 'report.html', // Nome do arquivo de relatório HTML
+  },
   e2e: {
-    // baseUrl
-
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
   },
 });
